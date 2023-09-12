80f9bc3759cee4915683b8b8730f8bbb8cab06bc
Folka2134 <folka2134@gmail.com>
2023-09-12 15:10:28 +0100

    Updated fetch function and created calculateCarRent function

--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,13 @@
-export async function fetchCars() {
+import { CarProps, FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters
+
   const headers = {
       'X-RapidAPI-Key': 'adb2be8597mshe0e0403c11a441cp1f919cjsna3e48edbd0da',
       'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+  const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel=${fuel}`, {
     headers: headers
   });
   
@@ -11,3 +15,18 @@ export async function fetchCars() {
 
   return result
 }
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
