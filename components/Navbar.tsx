fd69fc11047293845a11a02ce8681e5877a09adb
folka2134 <folka2134@gmail.com>
2024-03-22 13:09:00 +0000

    updated styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 const NavBar = () => (
-  <header className="w-full absolute z-10">
-    <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
-      <Link href="/" className="flex justify-center items-center">
+  <header className="absolute z-10 w-full">
+    <nav className="flex w-full items-center justify-between px-6 py-4 sm:px-24 md:px-52 ">
+      <Link href="/" className="flex items-center justify-center ">
         <Image
           src="/logo.png"
           alt="logo"
-          width={118}
-          height={18}
-          className="object-contain"
+          width={200}
+          height={20}
+          className="rounded-full bg-[#2B59FF] bg-opacity-80 object-contain"
         />
       </Link>
 
