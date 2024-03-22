b814ab82181ef7ef2c6f0fc88631f64290f95233
folka2134 <folka2134@gmail.com>
2024-03-22 13:07:12 +0000

    fixed auto scroll

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -28,7 +28,7 @@ const SearchManufacturer = ({
           {/* Button for the combobox. Click on the icon to see the complete dropdown */}
           <Combobox.Button className="absolute top-[14px]">
             <Image
-              src="/car-logo.png"
+              src="/logo.png"
               width={20}
               height={20}
               className="ml-4"
