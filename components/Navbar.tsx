7d3b66a23cd00e57866ff990c443b6a364a2c40a
folka2134 <folka2134@gmail.com>
2024-03-25 15:17:38 +0000

    updated styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const NavBar = () => (
   <header className="z-10 w-full">
-    <nav className="flex w-full items-center justify-between px-6 py-4 sm:px-24 md:px-52 ">
+    <nav className="flex w-full items-center justify-between px-6 py-4 sm:px-24 lg:px-36 xl:px-52">
       <Link href="/" className="flex items-center justify-center ">
         <Image
           src="/logo.png"
