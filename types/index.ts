a2df633ba74ec8d049afa9bc8168aa455cfda9a9
folka2134 <folka2134@gmail.com>
2024-03-25 16:57:07 +0000

    Made types files more readable

--- types/index.ts
+++ types/index.ts
@@ -1,20 +1,5 @@
 import { MouseEventHandler } from "react";
 
-export interface CarProps {
-  city_mpg: number;
-  class: string;
-  combination_mpg: number;
-  cylinders: number;
-  displacement: number;
-  drive: string;
-  fuel_type: string;
-  highway_mpg: number;
-  make: string;
-  model: string;
-  transmission: string;
-  year: number;
-}
-
 export interface FilterProps {
   manufacturer?: string;
   year?: number;
@@ -25,16 +10,6 @@ export interface FilterProps {
 
 export interface HomeProps {
   searchParams: FilterProps;
-}
-
-export interface CarCardProps {
-  model: string;
-  make: string;
-  mpg: number;
-  transmission: string;
-  year: number;
-  drive: string;
-  cityMPG: number;
 }
 
 export interface CustomButtonProps {
