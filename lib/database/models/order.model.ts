1ccedc191bead5469e29e8f0d39a558e42bacc5c
folka2134 <folka2134@gmail.com>
2024-06-11 11:31:31 +0100

    updated file name

--- lib/database/models/order.model.ts
+++ lib/database/models/order.model.ts
@@ -3,7 +3,7 @@ import { Schema, model, models } from "mongoose";
 export const OrderSchema = new Schema({
   carId: { type: String, required: true },
   userId: { type: String, required: true},
-  status: { type: String, required: true },
+  status: { type: String, default: "pending" },
   startDate: { type: String, default: new Date().toISOString()}
 })
 
