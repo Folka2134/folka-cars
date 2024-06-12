bd3b1d0dadcb04819881a32ded6223b3d96d1b6b
folka2134 <folka2134@gmail.com>
2024-06-12 15:45:50 +0100

    fixed buggs

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,4 +1,4 @@
-'use server'
+"use server"
 
 import { CheckoutParams, OrderParams } from "@/types/orderTypes";
 import { auth } from "@clerk/nextjs";
@@ -9,17 +9,22 @@ import { redirect } from "next/navigation";
 import Stripe from "stripe";
 
 export const checkoutOrder = async (order: CheckoutParams) => {
-
-
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
   try {
     const session = await stripe.checkout.sessions.create({
-      // line_items: [
-      //   {
-      //     quantity: 1
-      //   },
-      // ],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            unit_amount: 500,
+            product_data: {
+              name: order.carId.carId
+            }
+          },
+          quantity: 1
+        },
+      ],
       metadata: {
         carId: order.carId.carId,
         userId: order.userId,
@@ -27,12 +32,14 @@ export const checkoutOrder = async (order: CheckoutParams) => {
       mode: 'payment',
       success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
       cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
-    })
+      })
     redirect(session.url!)
   } catch (error) {
     throw error
   }
+        
 }
+
 
 export async function createOrder(order: OrderParams) {
   try {
