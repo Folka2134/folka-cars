8281a5c1a71240c2cc3ab5eec1fd528f47dc4706
folka2134 <folka2134@gmail.com>
2024-08-06 12:26:25 +0100

    displaying order details

--- components/OrderCard.tsx
+++ components/OrderCard.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 
 const OrderCard = ({ order }: any) => {
+  console.log(order);
+
+  return (
+    <div className="flex w-96 justify-center gap-5 border shadow-sm">
+      <h2>
+        {order.carMake} {order.carModel}
+      </h2>
+      <div className="flex flex-col gap-2">
+        <p>Start Date: {new Date(order.startDate).toLocaleDateString()}</p>
+        <p>Number of Days: {order.numberOfDays}</p>
+        <p>Total Cost: ${order.totalCost}</p>
+      </div>
+    </div>
+  );
 };
 
 export default OrderCard;
